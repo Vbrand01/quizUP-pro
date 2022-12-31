1f89fe45c4d540a7b850cd93e3d3849c041cdce8
Vbrand01 <viccorin20@gmail.com>
2022-12-31 01:35:04 -0300

    add-quiz

--- scripts/series/cobraKai/questions.js
+++ scripts/series/cobraKai/questions.js
@@ -1,11 +1,72 @@
 export default [
     {
-      question: "",
+      question: "Na terceira temporada, Johnny e Miguel curtem o show de qual lenda do rock?",
       answers: [
-        { option: "", correct: false },
-        { option: "", correct: false },
-        { option: "", correct: true },
-        { option: "", correct: false },
+        { option: "Dee Snider", correct: true },
+        { option: "Iggy Pop", correct: false },
+        { option: "Jon Bon Jovi", correct: false },
+        { option: "Alice Cooper", correct: false },
+      ],
+    },
+    {
+      question: "Qual dos filmes clássicos de Karatê Kid teve Terry Silver como vilão?",
+      answers: [
+        { option: "Karatê Kid - A Hora da Verdade (1984)", correct: false },
+        { option: "Karatê Kid 3 - O Desafio Final (1989)", correct: true },
+        { option: "Karatê Kid 2 - A Hora da Verdade Continua (1986)", correct: false },
+        { option: "Karatê Kid 4 - A Nova Aventura (1994)", correct: false },
+      ],
+    },
+    {
+      question: "Antes de Cobra Kai, um personagem de uma sitcom levantou a teoria de que Johnny Lawrence era o verdadeiro herói de Karatê Kid. Quem foi?",
+      answers: [
+        { option: "Chandler (Friends)", correct: false },
+        { option: "Abed (Community)", correct: false },
+        { option: "Howard (The Big Bang Theory)", correct: false },
+        { option: "Barney (How I Met Your Mother)", correct: true },
+      ],
+    },
+    {
+      question: "Como se chama o colégio em que Miguel, Hawk e os outros alunos estudam?",
+      answers: [
+        { option: "West Valley", correct: true },
+        { option: "Eagle Fang", correct: false },
+        { option: "San Fernando Valley", correct: false },
+        { option: "All Valley", correct: false },
+      ],
+    },
+    {
+      question: "Qual emissora criou Cobra Kai antes da série se mudar para a Netflix?",
+      answers: [
+        { option: "Amazon Prime Video", correct: false },
+        { option: "HBO", correct: false },
+        { option: "Starz", correct: false },
+        { option: "Youtube", correct: true },
+      ],
+    },
+    {
+      question: "Qual é o nome desse colega de Johnny Lawrence da antiga Cobra Kai, que aparece na série e nos filmes antigos?",
+      answers: [
+        { option: "Dutch", correct: false },
+        { option: "Jimmy", correct: false },
+        { option: "Tommy", correct: true },
+        { option: "Bobby", correct: false },
+      ],
+    },
+        {
+      question: "A atriz Elisabeth Shue viveu Ali em Karatê Kid, interesse romântico de Johnny e Daniel, e depois voltou para uma participação em Cobra Kai. Qual outra série a atriz estrela?",
+      answers: [
+        { option: "The Boys", correct: true },
+        { option: "The Witcher", correct: false },
+        { option: "Hunters", correct: false },
+        { option: "Hacks", correct: false },
+      ],
+    },
+        {
+      question: "Qual desses dois filmes de Karatê Kid não é considerado cânone para Cobra Kai?",
+      answers: [
+        { option: "Karatê Kid (2010)", correct: true },
+        { option: "Karatê Kid 4 - A Nova Aventura (1994)", correct: false },
       ],
     },
     
