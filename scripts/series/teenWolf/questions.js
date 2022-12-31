fcebe6b4c780713f1b1bfd04183e0c5fd573f99b
Vbrand01 <viccorin20@gmail.com>
2022-12-31 02:05:27 -0300

    add-quiz

--- scripts/series/teenWolf/questions.js
+++ scripts/series/teenWolf/questions.js
@@ -1,11 +1,87 @@
 export default [
     {
-      question: "",
+      question: "Scott Mccall nunca foi mordido?",
       answers: [
-        { option: "", correct: false },
-        { option: "", correct: false },
-        { option: "", correct: true },
-        { option: "", correct: false },
+        { option: "Falso", correct: true },
+        { option: "Verdadeiro", correct: false },
+      ],
+    },
+    {
+      question: "A série foi desenvolvida por Jeff Davis.",
+      answers: [
+        { option: "Verdadeiro", correct: true },
+        { option: "Falso", correct: false },
+      ],
+    },
+    {
+      question: "Teen Wolf estreou quando?",
+      answers: [
+        { option: "5 de Junho de 2011", correct: true },
+        { option: "5 de Junho de 2009", correct: false },
+        { option: "5 de Junho de 2012", correct: false },
+        { option: "Nenhuma das alternativas acima.", correct: false },
+      ],
+    },
+    {
+      question: "O nome do personagem principal na série é:",
+      answers: [
+        { option: "Scott Gregorio McCall", correct: false },
+        { option: "Scott Call", correct: false },
+        { option: "Scott Gregorio Howard McCall", correct: true },
+        { option: "Scott McCall", correct: false },
+      ],
+    },
+    {
+      question: "Scott foi mordido por um lobisomem numa noite enquanto procurava um corpo junto com seu melhor amigo Derek Hale",
+      answers: [
+        { option: "Verdadeiro", correct: false },
+        { option: "Falso", correct: true },
+      ],
+    },
+    {
+      question: "Derek Hale tem o ajudado a proteger Beacon Hills de inimigos Sobrenaturais.",
+      answers: [
+        { option: "Verdadeiro", correct: true },
+        { option: "Falso", correct: false },
+      ],
+    },
+    {
+      question: "A primeira aparição de Derek Hale foi em 'A lua crescente'",
+      answers: [
+        { option: "Verdadeiro", correct: false },
+        { option: "Falso", correct: true },
+      ],
+    },
+    {
+      question: "Lydia foi mordida por",
+      answers: [
+        { option: "Scott", correct: false },
+        { option: "Derek", correct: false },
+        { option: "Isaac", correct: false },
+        { option: "Peter", correct: true },
+      ],
+    },
+    {
+      question: "Lydia é mordida em qual temporada?",
+      answers: [
+        { option: "4", correct: false },
+        { option: "3", correct: false },
+        { option: "2", correct: true },
+        { option: "1", correct: false },
+      ],
+    },
+    {
+      question: "Scott faz vários inimigos, os quais ameaçam a segurança de seus entes queridos, e também os demais moradores de Beacon Hills.",
+      answers: [
+        { option: "Verdadeiro", correct: true },
+        { option: "Falso", correct: false },
+      ],
+    },
+    {
+      question: "Allison descobre que Scott é um lobisomem,mas isso acaba fazendo ela mudar seus sentimentos românticos sobre ele.",
+      answers: [
+        { option: "Verdadeiro", correct: false },
+        { option: "Falso", correct: true },
       ],
     },
     
